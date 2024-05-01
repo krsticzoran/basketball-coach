355028cca5e2dd3f1a84c5bc7a3e0cbbaa87bca5
zorannkrstic <zorankrstic81@gmail.com>
2024-05-01 12:04:46 +0200

    prettier & eslint config

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Basketball Coach Siniša Savović ",
+  title: 'Basketball Coach Siniša Savović ',
   description:
-    "Siniša Savović, a seasoned basketball coach, has trained numerous renowned players, including NBA MVP Nikola Jokić. Offering tailored individual training sessions.",
-};
+    'Siniša Savović, a seasoned basketball coach, has trained numerous renowned players, including NBA MVP Nikola Jokić. Offering tailored individual training sessions.',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
