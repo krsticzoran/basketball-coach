b9643414040a5d57c16d9f6562d258309b094b2c
zorannkrstic <zorankrstic81@gmail.com>
2024-05-03 06:48:40 +0200

    mixin @media

--- app/page.js
+++ app/page.js
@@ -1,11 +1,3 @@
-import Link from 'next/link'
-
 export default function Home() {
-  return (
-    <div className='container'>
-      <Link href='/' style={{ color: 'red' }}>
-        Hello World!!!
-      </Link>
-    </div>
-  )
+  return <main></main>
 }
