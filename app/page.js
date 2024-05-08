34279a929b1854fbb3cb2e24915e232354dd20aa
zorannkrstic <zorankrstic81@gmail.com>
2024-05-09 00:01:37 +0200

    font size

--- app/page.js
+++ app/page.js
@@ -4,8 +4,13 @@ import bgImage from '../public/background.jpg'
 export default function Home() {
   return (
     <main style={{ display: 'flex' }}>
-      <div style={{ width: '50%' }} className='pb-25'>
-        Some text will go here
+      <div style={{ width: '50%' }}>
+        <h1>Ready to Train like an MVP? </h1>
+        <h5>
+          <span> Work with a coach who trained Jokic to success!</span>
+          <span>Train with Sinisa Savovic now</span>
+        </h5>
+        <button>Book Your Session</button>
       </div>
       <Image
         src={bgImage}
