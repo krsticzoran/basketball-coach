ef5eea763254c0a3c82dcf14460a50451ef7d808
zorannkrstic <zorankrstic81@gmail.com>
2024-05-15 10:01:54 +0200

    hero section style

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
-import HeroSection from './hero-section'
+import Image from 'next/image'
+import bgImage from '../public/jokic3.jpeg'
+import Button from '@/components/ui/button'
+import classes from '@/styles/pages/home.module.scss'
 
 export default function Home() {
   return (
     <main>
-      <HeroSection />
+      <section className={classes.hero}>
+        <div className={classes.text}>
+          <h1>Ready to Train like an MVP? </h1>
+          <h5>
+            <span> Work with a coach who trained Jokic to success!</span>
+            <span>Train with Sinisa Savovic now</span>
+          </h5>
+          <Button href='/contact/' color='dark'>
+            Book Your Session
+          </Button>
+        </div>
+        <div>
+          <Image src={bgImage} priority alt='Nikola Jokic' />
+        </div>
+      </section>
     </main>
   )
 }
