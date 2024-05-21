cc1e57fcd433cd43e0cee863bada01be8c983348
zorannkrstic <zorankrstic81@gmail.com>
2024-05-21 08:46:47 +0200

    about section

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import bgImage from '../public/jokic.jpeg'
 import Button from '@/components/ui/button'
 import classes from '@/styles/pages/home.module.scss'
+import welcomeImg from '../public/Savovic1.jpg'
 
 export default function Home() {
   return (
@@ -22,6 +23,30 @@ export default function Home() {
         </div>
         <div>
           <Image src={bgImage} priority alt='Nikola Jokic' />
+        </div>
+      </section>
+      <section className={classes.welcome}>
+        <div className={classes.container}>
+          <div>
+            <Image src={welcomeImg} alt='Sinisa Savovic' />
+          </div>
+
+          <div>
+            <div className={classes.subtitle}>
+              <div className={classes.line}></div>
+              <div className={classes.text}>about</div>
+            </div>
+            <h2>Meet Sinisa Savovic: A Trailblazer in Basketball Excellence</h2>
+            <p>
+              Sinisa Savovic, a visionary leader in the realm of basketball
+              coaching, embodies a legacy of unparalleled expertise and
+              dedication. With a career spanning decades, his influence has
+              shaped the trajectories of countless athletes, including the likes
+              of NBA star Nikola Jokic. Dive deeper into Sinisa's journey,
+              philosophy, and unparalleled impact in the world of basketball.
+            </p>
+            <Button href='/about/'>Find Out More</Button>
+          </div>
         </div>
       </section>
     </main>
