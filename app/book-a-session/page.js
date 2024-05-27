1349040a13a8329fc96ad09ec8dad33d586a835d
zorannkrstic <zorankrstic81@gmail.com>
2024-05-27 19:49:23 +0200

    contact form

--- app/book-a-session/page.js
+++ app/book-a-session/page.js
@@ -1,7 +1,30 @@
+import classes from '@/styles/pages/book-a-session.module.scss'
+
 export default function Contact() {
   return (
-    <>
-      <p>Contact</p>
-    </>
+    <div className={classes.contact}>
+      <div className={classes.data}>
+        <form>
+          <input type='text' name='name' placeholder='First Name' required />
+          <input type='text' name='surname' placeholder='Last Name' required />
+          <input
+            type='email'
+            name='email'
+            placeholder='Email'
+            className={classes[`full-width`]}
+            required
+          />
+          <input
+            type='tel'
+            name='tel'
+            placeholder='Phone Number'
+            className={classes[`full-width`]}
+            required
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+      <div></div>
+    </div>
   )
 }
