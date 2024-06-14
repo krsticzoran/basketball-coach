fb1c005b7451b454f59e926864197d315e49f782
zorannkrstic <zorankrstic81@gmail.com>
2024-06-14 10:19:38 +0200

    svg

--- components/ui/accordion.js
+++ components/ui/accordion.js
@@ -3,9 +3,6 @@
 import { useState } from 'react'
 import classes from '@/styles/components/ui/accordion.module.scss'
 
-import close from '../../public/close.svg'
-import Image from 'next/image'
-
 export default function Accordion({ header, content }) {
   const [isShowing, setIsShowing] = useState(false)
 
@@ -21,11 +18,17 @@ export default function Accordion({ header, content }) {
         <h6>{header}</h6>
 
         <div className={classes.title}>
-          <Image
-            src={close}
-            alt='close'
+          <svg
             className={isShowing ? classes.open : classes.close}
-          />
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='currentColor'
+            width='24px'
+            height='24px'
+          >
+            <path d='M0 0h24v24H0z' fill='none' />
+            <path d='M18.3 5.71a.996.996 0 00-1.41 0L12 10.59 7.11 5.7a.996.996 0 10-1.41 1.41L10.59 12l-4.89 4.89a.996.996 0 101.41 1.41L12 13.41l4.89 4.89a.996.996 0 101.41-1.41L13.41 12l4.89-4.89c.39-.38.39-1.02 0-1.4z' />
+          </svg>
         </div>
       </div>
       <div className={content}>
