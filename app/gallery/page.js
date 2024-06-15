52d1c977c083f9887104fe0b2e277f9b41e3016a
zorannkrstic <zorankrstic81@gmail.com>
2024-06-15 10:03:02 +0200

    gallery

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,7 +1,40 @@
+import Image from 'next/image'
+import classes from '@/styles/pages/gallery.module.scss'
+
+const images = [
+  { path: '/Savovic.jpeg' },
+  { path: '/Savovic1.jpg' },
+  { path: '/jokic.jpeg' },
+  { path: '/Savovic1.jpg' },
+  { path: '/jokic.jpeg' },
+  { path: '/jokic.jpeg' },
+  { path: '/Savovic.jpeg' },
+  { path: '/Savovic1.jpg' },
+  { path: '/jokic.jpeg' },
+  { path: '/Savovic1.jpg' },
+  { path: '/jokic.jpeg' },
+  { path: '/jokic.jpeg' },
+  { path: '/Savovic.jpeg' },
+  { path: '/Savovic1.jpg' },
+  { path: '/jokic.jpeg' },
+  { path: '/Savovic1.jpg' },
+  { path: '/jokic.jpeg' },
+  { path: '/jokic.jpeg' },
+]
+
 export default function Gallery() {
   return (
-    <>
-      <p>Gallery</p>
-    </>
+    <div className={classes.gallery}>
+      {images.map((image, index) => (
+        <div key={index} className={classes['gallery-item']}>
+          <Image
+            src={image.path}
+            alt={`Image ${index + 1}`}
+            width={500}
+            height={300}
+          />
+        </div>
+      ))}
+    </div>
   )
 }
