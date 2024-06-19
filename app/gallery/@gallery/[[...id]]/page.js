c989a888275503d86eab633656376fa765a81c51
zorannkrstic <zorankrstic81@gmail.com>
2024-06-19 14:05:08 +0200

    svg close

--- app/gallery/@gallery/[[...id]]/page.js
+++ app/gallery/@gallery/[[...id]]/page.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import classes from '@/styles/pages/gallery.module.scss'
 import { getImage } from '@/lib/get-images'
+import SvgClose from '@/components/ui/svg-close'
 
 export default async function Gallery({ params }) {
   const data = await getImage()
@@ -18,16 +19,7 @@ export default async function Gallery({ params }) {
           <div className={classes.container}>
             <button>
               <Link href={'/gallery'}>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 24 24'
-                  fill='currentColor'
-                  width='24px'
-                  height='24px'
-                >
-                  <path d='M0 0h24v24H0z' fill='none' />
-                  <path d='M18.3 5.71a.996.996 0 00-1.41 0L12 10.59 7.11 5.7a.996.996 0 10-1.41 1.41L10.59 12l-4.89 4.89a.996.996 0 101.41 1.41L12 13.41l4.89 4.89a.996.996 0 101.41-1.41L13.41 12l4.89-4.89c.39-.38.39-1.02 0-1.4z' />
-                </svg>
+                <SvgClose />
               </Link>
             </button>
             <Image
