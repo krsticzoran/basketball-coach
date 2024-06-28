1afc012caa8f22024ac000679d0cd018ccb22509
zorannkrstic <zorankrstic81@gmail.com>
2024-06-28 20:22:30 +0200

    about images

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,18 @@
+import classes from '@/styles/pages/about.module.scss'
+import Image from 'next/image'
+import trainingSession from '../../public/session.jpeg'
+
 export default function About() {
   return (
-    <>
-      <p>about</p>
-    </>
+    <div className={classes.about}>
+      <div className={classes.hero}>
+        <h1>about Siniša</h1>
+      </div>
+      <div className={classes.images}>
+        <Image src={trainingSession} alt='training session' />
+        <Image src={trainingSession} alt='training session' />
+        <Image src={trainingSession} alt='training session' />
+      </div>
+    </div>
   )
 }
