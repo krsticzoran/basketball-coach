85597f867b6ba52d3e9b5f8adadff06b28d62799
zorannkrstic <zorankrstic81@gmail.com>
2024-06-29 20:08:45 +0200

    my stoory

--- app/about/page.js
+++ app/about/page.js
@@ -13,6 +13,31 @@ export default function About() {
         <Image src={trainingSession} alt='training session' />
         <Image src={trainingSession} alt='training session' />
       </div>
+      <div className={classes.story}>
+        <div className={classes.content}>
+          <div className={classes.heading}>
+            <div className='subtitle'>
+              <div className='line'></div>
+              <div className='text'>my story</div>
+              <div className='line'></div>
+            </div>
+            <h2>Siniša's Coaching Journey</h2>
+          </div>
+          <div className={classes.text}>
+            <p>
+              Neque ornare pellentesque mus in nisi, mi, ac. Morbi volutpat
+              velit id adipiscing mollis. Quisque bibendum vehicula eget ac
+              fringilla non nec. Sed vitae faucibus tristique nisl, tincidunt
+              nec consequat ullamcorper. Eu volutpat lacus lacus porttitor sed
+              nunc. <br /> <br />
+              Sed nunc vel massa eget egestas. Non sapien cursus risus lorem.
+              Adipiscing eu a penatibus ac viverra cras diam feugiat ornare.
+              Gravida nibh at bibendum cras massa. Vitae proin convallis enim
+              nunc libero. Lacus aenean vestibulum egestas nec velit sed.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
