80463a3de2347ff14a5e2068386309e26a34ae0a
zorannkrstic <zorankrstic81@gmail.com>
2024-06-30 11:31:28 +0200

    about

--- app/about/page.js
+++ app/about/page.js
@@ -1,14 +1,17 @@
 import classes from '@/styles/pages/about.module.scss'
 import Image from 'next/image'
 import trainingSession from '../../public/session.jpeg'
+import Button from '@/components/ui/button'
 
 export default function About() {
   return (
     <div className={classes.about}>
       <div className={classes.hero}>
-        <h1>about Siniša</h1>
+        <h1>
+          about <span>Siniša</span>
+        </h1>
       </div>
-      <div className={classes.images}>
+      <div className={`${classes['images-three']} ${classes.images}`}>
         <Image src={trainingSession} alt='training session' />
         <Image src={trainingSession} alt='training session' />
         <Image src={trainingSession} alt='training session' />
@@ -36,6 +39,52 @@ export default function About() {
               nunc libero. Lacus aenean vestibulum egestas nec velit sed.
             </p>
           </div>
+        </div>
+      </div>
+      <div className={`${classes['images-two']} ${classes.images}`}>
+        <Image src={trainingSession} alt='training session' />
+        <Image src={trainingSession} alt='training session' />
+      </div>
+      <div className={classes.story}>
+        <div className={classes.content}>
+          <div className={classes.heading}>
+            <div className='subtitle'>
+              <div className='line'></div>
+              <div className='text'>My misson</div>
+              <div className='line'></div>
+            </div>
+            <h2>ATTAIN MAXIMUM PERFORMANCE</h2>
+          </div>
+          <div className={classes.text}>
+            <p>
+              Risus velit mollis netus sollicitudin at duis adipiscing ac. Amet
+              porttitor consectetur nunc, sit placerat dictum. Ut faucibus
+              pharetra sed habitant duis convallis quam quis non. Lectus
+              fringilla nunc sed faucibus. Integer sapien, id tempor magna
+              integer sit orci. Vel montes, ante integer diam nisl placerat
+              auctor vitae felis. Aliquet vitae lacus, consectetur fusce nunc
+              placerat mi. <br /> <br />
+              Lectus vitae tellus vel ac sagittis integer. Integer mi tristique
+              gravida purus amet tincidunt magna etiam. Viverra in tristique
+              varius duis egestas bibendum. A mattis ultrices egestas maecenas
+              in at euismod. Ipsum, ac in consectetur felis mauris.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={`${classes['images-three']} ${classes.images}`}>
+        <Image src={trainingSession} alt='training session' />
+        <Image src={trainingSession} alt='training session' />
+        <Image src={trainingSession} alt='training session' />
+      </div>
+      <div className={classes.session}>
+        <div className={classes.content}>
+          <h2>
+            <span> BOOK A SESSION </span>
+            and start your <br /> journey today!
+          </h2>
+
+          <Button href='/contact/'>get started</Button>
         </div>
       </div>
     </div>
