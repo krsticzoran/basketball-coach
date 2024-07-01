45f1cfbfc613510e9c7842cdd93830544e49630a
zorannkrstic <zorankrstic81@gmail.com>
2024-07-01 11:32:33 +0200

    resend config

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,9 +1,20 @@
 import { NextResponse } from 'next/server'
+import { Resend } from 'resend'
 
 export async function GET() {
+  const resend = new Resend(process.env.RESEND_API_KEY)
   try {
-    return NextResponse.json({ hello: 'world' })
+    console.log('Attempting to send email...')
+    const { data } = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: 'zorankrstic81@gmail.com',
+      subject: 'Hello',
+      html: '<h1>It will work</h1>',
+    })
+    console.log('Email sent successfully:', data)
+    return NextResponse.json({ data })
   } catch (error) {
+    console.error('Error sending email:', error.message)
     return NextResponse.json({ error: error.message })
   }
 }
