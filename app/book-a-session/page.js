a590b3c7ab603409cd163cbcdbb3c22f67f27d47
zorannkrstic <zorankrstic81@gmail.com>
2024-07-08 10:18:14 +0200

    reset form

--- app/book-a-session/page.js
+++ app/book-a-session/page.js
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState, useEffect, useRef } from 'react'
 import { useFormState } from 'react-dom'
 
 import classes from '@/styles/pages/book-a-session.module.scss'
@@ -16,6 +17,20 @@ const initialState = {
 
 export default function Contact() {
   const [state, formAction] = useFormState(sendingEmail, initialState)
+  const [message, setMessage] = useState(state.message)
+  const formRef = useRef(null)
+
+  const handleFocus = () => {
+    setMessage('')
+  }
+
+  useEffect(() => {
+    setMessage(state.message)
+    if (state.message === 'Your message has been sent successfully.') {
+      formRef.current.reset()
+    }
+  }, [state])
+
   return (
     <>
       <div className={classes.contact}>
@@ -30,13 +45,20 @@ export default function Contact() {
             shortly to create a personalized plan for your success.
           </p>
 
-          <form action={formAction}>
-            <input type='text' name='name' placeholder='First Name' required />
+          <form action={formAction} ref={formRef}>
+            <input
+              type='text'
+              name='name'
+              placeholder='First Name'
+              required
+              onFocus={handleFocus}
+            />
             <input
               type='text'
               name='surname'
               placeholder='Last Name'
               required
+              onFocus={handleFocus}
             />
             <input
               type='email'
@@ -44,6 +66,7 @@ export default function Contact() {
               placeholder='Email'
               className={classes[`full-width`]}
               required
+              onFocus={handleFocus}
             />
             <input
               type='tel'
@@ -51,16 +74,18 @@ export default function Contact() {
               placeholder='Phone Number'
               className={classes[`full-width`]}
               required
+              onFocus={handleFocus}
             />
             <textarea
               placeholder='Message'
               name='message'
               className={classes[`full-width`]}
               required
+              onFocus={handleFocus}
             ></textarea>
             <FormSubmitButton />
           </form>
-          <p>{state?.message}</p>
+          <p>{message}</p>
         </div>
         <div className={classes['image-container']}>
           <Image src={trainingSession} alt='training session' />
