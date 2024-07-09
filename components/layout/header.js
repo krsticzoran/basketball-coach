6a912b95bc039a386e5fa94a1895c9af7cd66917
zorannkrstic <zorankrstic81@gmail.com>
2024-07-09 12:07:51 +0200

    isDesktop logic

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,39 +1,67 @@
+'use client'
+
 import Link from 'next/link'
 import classes from '@/styles/components/layout/header.module.scss'
 import instagram from '../../public/instagram.png'
 import Image from 'next/image'
 import NavLink from './nav-link'
+import { useState, useEffect } from 'react'
 
 export default function Header() {
-  return (
-    <header className={classes.header}>
-      <ul>
-        <li className={classes.logo}>
-          <Link href='/'>some logo</Link>
-        </li>
-      </ul>
+  const [isDesktop, setIsDesktop] = useState(null)
 
-      <nav>
-        <ul>
-          <li>
-            <NavLink href='/'>Home</NavLink>
-          </li>
-          <li>
-            <NavLink href='/about'>About</NavLink>
-          </li>
-          <li>
-            <NavLink href='/gallery'>Gallery</NavLink>
-          </li>
-          <li className={classes.session}>
-            <Link href='/book-a-session'>book a session</Link>
-          </li>
-          <li>
-            <Link href='https://www.instagram.com/sawa_ska/'>
-              <Image src={instagram} priority alt='Instagram' />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+  const checkWindowWidth = () => {
+    if (typeof window !== 'undefined') {
+      const windowWidth = window.innerWidth
+      setIsDesktop(windowWidth >= 1024)
+    }
+  }
+
+  useEffect(() => {
+    checkWindowWidth()
+    window.addEventListener('resize', checkWindowWidth)
+    return () => window.removeEventListener('resize', checkWindowWidth)
+  }, [])
+
+  return (
+    <>
+      {isDesktop === null ? (
+        <div style={{ visibility: 'hidden' }}>Loading...</div>
+      ) : isDesktop ? (
+        <header className={classes.header}>
+          <ul>
+            <li className={classes.logo}>
+              <Link href='/'>some logo</Link>
+            </li>
+          </ul>
+
+          <nav>
+            <ul>
+              <li>
+                <NavLink href='/'>Home</NavLink>
+              </li>
+              <li>
+                <NavLink href='/about'>About</NavLink>
+              </li>
+              <li>
+                <NavLink href='/gallery'>Gallery</NavLink>
+              </li>
+              <li className={classes.session}>
+                <Link href='/book-a-session'>book a session</Link>
+              </li>
+              <li>
+                <Link href='https://www.instagram.com/sawa_ska/'>
+                  <Image src={instagram} priority alt='Instagram' />
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      ) : (
+        <>
+          <p>header for mobile</p>
+        </>
+      )}
+    </>
   )
 }
