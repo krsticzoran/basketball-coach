1f970271e33c34499a0af04aa4f798989fb76453
zorannkrstic <zorankrstic81@gmail.com>
2024-09-06 13:12:56 +0200

    the slider no longer exists

--- components/layout/modal.js
+++ components/layout/modal.js
@@ -1,30 +1,9 @@
-'use client'
 import SvgClose from '@/components/ui/svg-close'
-import ArrowIcon from '@/components/ui/arrow-icon'
 import classes from '@/styles/pages/gallery.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 
 export default function Modal({ params, data }) {
-  const router = useRouter()
-
-  const handlePrevClick = () => {
-    setTimeout(() => {
-      router.push(
-        `/gallery/${params.id == 0 ? data.length - 1 : params.id - 1}`,
-      )
-    }, '300')
-  }
-
-  const handleNextClick = () => {
-    setTimeout(() => {
-      router.push(
-        `/gallery/${params.id == data.length - 1 ? 0 : +params.id + 1}`,
-      )
-    }, '300')
-  }
-
   return (
     <>
       {params.id && (
@@ -34,13 +13,6 @@ export default function Modal({ params, data }) {
               <Link href={'/gallery'}>
                 <SvgClose />
               </Link>
-            </button>
-            <button className={classes.left} onClick={handlePrevClick}>
-              <ArrowIcon direction='left' />
-            </button>
-
-            <button className={classes.right} onClick={handleNextClick}>
-              <ArrowIcon direction='right' />
             </button>
             <div>
               <Image
