3350680287bba1c750ecbe58baffdca8894eed2d
zorannkrstic <zorankrstic81@gmail.com>
2024-10-05 09:04:00 +0200

    delay 0.1

--- components/layout/header.js
+++ components/layout/header.js
@@ -8,6 +8,7 @@ import Image from 'next/image'
 import NavLink from './nav-link'
 import { useState, useEffect } from 'react'
 import logo from '@/public/logo.png'
+import ContentReveal from '../ui/content-reveal'
 
 export default function Header() {
   const [isDesktop, setIsDesktop] = useState(null)
@@ -31,43 +32,8 @@ export default function Header() {
       {isDesktop === null ? (
         <div style={{ visibility: 'hidden' }}>Loading...</div>
       ) : isDesktop ? (
-        <header className={classes.header}>
-          <ul>
-            <li className={classes.logo}>
-              <Link href='/'>
-                <Image src={logo} priority alt='logo' />
-              </Link>
-            </li>
-          </ul>
-
-          <nav>
-            <ul>
-              <li>
-                <NavLink href='/'>Home</NavLink>
-              </li>
-              <li>
-                <NavLink href='/about'>About</NavLink>
-              </li>
-              <li>
-                <NavLink href='/gallery'>Gallery</NavLink>
-              </li>
-              <li className={classes.session}>
-                <Link href='/book-a-session'>book a session</Link>
-              </li>
-              <li>
-                <Link
-                  href='https://www.instagram.com/sawa_ska/'
-                  className={classes.instangram}
-                >
-                  <Image src={instagram} priority alt='Instagram' />
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-      ) : (
-        <>
-          <header className={classes['header-mobile']}>
+        <ContentReveal delay={0.1}>
+          <header className={classes.header}>
             <ul>
               <li className={classes.logo}>
                 <Link href='/'>
@@ -75,28 +41,67 @@ export default function Header() {
                 </Link>
               </li>
             </ul>
-            <ul>
-              <li
-                className={
-                  isOpen
-                    ? classes['menu-button-open']
-                    : classes['menu-button-close']
-                }
-              >
-                <button onClick={() => setIsOpen((prevState) => !prevState)}>
-                  <Image src={menu} priority alt='menu icon' />
-                </button>
-              </li>
-              <li>
-                <Link
-                  href='https://www.instagram.com/sawa_ska/'
-                  className={classes.instangram}
-                >
-                  <Image src={instagram} priority alt='Instagram' />
-                </Link>
-              </li>
-            </ul>
+
+            <nav>
+              <ul>
+                <li>
+                  <NavLink href='/'>Home</NavLink>
+                </li>
+                <li>
+                  <NavLink href='/about'>About</NavLink>
+                </li>
+                <li>
+                  <NavLink href='/gallery'>Gallery</NavLink>
+                </li>
+                <li className={classes.session}>
+                  <Link href='/book-a-session'>book a session</Link>
+                </li>
+                <li>
+                  <Link
+                    href='https://www.instagram.com/sawa_ska/'
+                    className={classes.instangram}
+                  >
+                    <Image src={instagram} priority alt='Instagram' />
+                  </Link>
+                </li>
+              </ul>
+            </nav>
           </header>
+        </ContentReveal>
+      ) : (
+        <>
+          <ContentReveal delay={0.1}>
+            <header className={classes['header-mobile']}>
+              <ul>
+                <li className={classes.logo}>
+                  <Link href='/'>
+                    <Image src={logo} priority alt='logo' />
+                  </Link>
+                </li>
+              </ul>
+              <ul>
+                <li
+                  className={
+                    isOpen
+                      ? classes['menu-button-open']
+                      : classes['menu-button-close']
+                  }
+                >
+                  <button onClick={() => setIsOpen((prevState) => !prevState)}>
+                    <Image src={menu} priority alt='menu icon' />
+                  </button>
+                </li>
+                <li>
+                  <Link
+                    href='https://www.instagram.com/sawa_ska/'
+                    className={classes.instangram}
+                  >
+                    <Image src={instagram} priority alt='Instagram' />
+                  </Link>
+                </li>
+              </ul>
+            </header>
+          </ContentReveal>
           <div
             className={`${classes['menu-modal']} ${isOpen ? classes.open : classes.hidden}`}
           >
